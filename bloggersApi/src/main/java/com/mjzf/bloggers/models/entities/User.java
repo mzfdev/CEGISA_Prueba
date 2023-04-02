561915e03aebc320a3f4272a10b6c22253fde0a4
MJZF00155619 <00155619@uca.edu.sv>
2023-04-02 12:56:37 -0600

    :bug: Fix: Fixing Spring Security Errors and Updating WebSecurity

--- bloggersApi/src/main/java/com/mjzf/bloggers/models/entities/User.java
+++ bloggersApi/src/main/java/com/mjzf/bloggers/models/entities/User.java
@@ -2,6 +2,8 @@ package com.mjzf.bloggers.models.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity(name = "user_n")
@@ -10,6 +12,7 @@ public class User {
 	//Atributes
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
 	private Long code;
 	
